af43541685f27f313198a5927de97b2a7e94f766
krishnapalsonaniya29 <krishnnapalsonaniya@gmail.com>
2025-12-28 12:26:47 +0530

    added vercel.json, removed commented code

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,29 +1,3 @@
-// const BASE_URL = "http://localhost:5000/api";
-
-// const getToken = () => localStorage.getItem("token");
-
-// export const apiFetch = async (endpoint, options = {}) => {
-//   const res = await fetch(`${BASE_URL}${endpoint}`, {
-//     headers: {
-//       Authorization: `Bearer ${getToken()}`,
-//       ...options.headers,
-//     },
-//     ...options,
-//   });
-
-//   let data;
-//   try {
-//     data = await res.json();
-//   } catch {
-//     throw new Error("Invalid server response");
-//   }
-
-//   if (!res.ok) {
-//     throw new Error(data?.message || "Something went wrong");
-//   }
-
-//   return data;
-// };
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const apiFetch = async (endpoint, options = {}) => {
